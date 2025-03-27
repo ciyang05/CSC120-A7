6428cd70a1a302047e9c493b2bdf89f411022b94
cyang-05 <chiashiiyang@gmail.com>
2025-03-27 11:54:43 -0400

    almost done w/house

--- House.java
+++ House.java
@@ -9,15 +9,15 @@ public class House extends Building {
 
 
   /**
-   * creates an instance of a house
+   * the constructor, which creates an instance of a house
    * @param name
    * @param address
    * @param nFloors
    * @param hasDiningRoom
    */
-  public House(String name, String address, int nFloors, boolean hasDiningRoom) {
-    super(name, address, nFloors);
-    this.residents = new ArrayList<String>(); 
+  public House(String name, String address, int nFloors, boolean hasDiningRoom) { // still passing in boolean, but it won't be used in super
+    super(name, address, nFloors); // creates a house using the Building class, must use same parameters as Building constructor
+    this.residents = new ArrayList<String>(); // <String> indicates what kind of data will be stored in ArrayList
     this.hasDiningRoom = hasDiningRoom;
 
     System.out.println("You have built a house: 🏠");
@@ -40,23 +40,50 @@ public class House extends Building {
     return this.residents.size();
   }
 
+
+  /**
+   * updates the "residents" arraylist everytime someone moves in
+   * @param name
+   */
   public void moveIn(String name) {
+    residents.add(name); // Do I have to include this in front of residents?
 
   }
 
+
+  /**
+   * updates the "residents" arraylist everytime someone moves out
+   * @param name
+   * @return name of person who moved out
+   */
   public String moveOut(String name) {
+    residents.remove(name);
+    return name;
 
   }
 
-  public Boolean isAResident(String name) {
-    if (residents.contains(name)) {
-      
+
+  /**
+   * checks if a given person is a resident of the "House"
+   * @param person
+   * @return true or false depending on if the person is a resident
+   */
+  public Boolean isResident(String person) {
+    if (residents.contains(person)) {
+      return true;
+    } else {
+      return false;
     }
   } 
 
 
+  /**
+   * the main function for "House" class, which calls the constructor to initialize a new house
+   * @param args
+   */
   public static void main(String[] args) {
-    new House();
+   House myHouse = new House("Cutter", "1 Prospect St", 4, true);
+   System.out.println(myHouse);
   }
 
 }
