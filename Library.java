3ed911b39cd133d70bad4f09834a35b9bc05cbc0
kiaras-create <ksunilsingh@smith.edu>
2025-03-27 13:54:54 -0400

    library updates

--- Library.java
+++ Library.java
@@ -1,12 +1,89 @@
+import java.util.Hashtable;
 /* This is a stub for the Library class */
-public class Library {
+public class Library extends Building implements LibraryRequirements {
 
-    public Library() {
-      System.out.println("You have built a library: 📖");
-    }
-  
+// Attributes
+private Hashtable<String, Boolean> collection;
+
+/**
+ * @param name
+ * @param address
+ * @param nFloors
+ */
+public Library(String name, String address, int nFloors ) {
+  super(name, address, nFloors);
+  System.out.println("You have built a library: 📖");
+  this.collection = new Hashtable<String, Boolean>();
+}
+
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
-      new Library();
+      Library myLibrary = new Library("Kiara's library", "123 St", 3);
+    }
+    
+    @Override
+    public void addTitle(String title) {
+      collection.put(title, true);
+      System.out.println(title + "has been added to the library collection");
+    }
+    /**
+     * adds specific title to the collection hashtable 
+     * @return title of the book added to the collection
+     */
+    @Override
+    public String removeTitle(String title) {
+      collection.remove(title);
+      return title;
+    }
+    
+    @Override
+    public void checkOut(String title) {
+    if (collection.containsKey(title)){
+      collection.replace(title, false);
+    } else {
+      System.out.println("Book not available");
+    }
+    }
+
+    @Override
+    public void returnBook(String title) {
+    if (collection.containsKey(title)) {
+      collection.replace(title, true);
+    } else {
+      System.out.println("Book not available");
+    }
+    }
+    /**
+     * checks if title is contained within the collection
+     * @return true or false depending on if the collection contains the title
+     */
+    @Override
+    public boolean containsTitle(String title) {
+      if (collection.containsKey(title)) {
+        return true;  
+      } else {
+        return false;
+      }
+    }
+     /**
+     * Checks if title is available in collection 
+     * @return true or false depending on whether the title is available in the collection
+     */
+    @Override
+    public boolean isAvailable(String title) {
+      if (collection.containsKey(title)){
+        return collection.get(title) ;
+      } else {
+        return false;
+      }
+    }
+
+    @Override
+    public boolean printCollection() {
+     System.out.println(collection.toString());
+     return true;
     }
   
   }
